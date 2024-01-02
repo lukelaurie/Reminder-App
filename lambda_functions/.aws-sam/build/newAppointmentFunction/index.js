aaf74b0e6cc4f6c1c25931289ea40a458b5d7ecb
unknown <lukelaurie13@gmail.com>
2024-01-01 20:10:23 -0700

    lambdas working locally

---
+++ lambda_functions/.aws-sam/build/newAppointmentFunction/index.js
@@ -0,0 +1,9 @@
+exports.lambdaHandler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from new appointment!'),
+    };
+    return response;
+  };
+  
---
+++ lambda_functions/.aws-sam/build/sendAppointmentRemindersFunction/index.js
@@ -0,0 +1,9 @@
+exports.lambdaHandler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from send remindeers!'),
+    };
+    return response;
+  };
+  
