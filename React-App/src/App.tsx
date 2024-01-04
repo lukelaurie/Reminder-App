bb392ca0b45966a5c0c9a8f4c9424aaaea52de16
Desmondtga <Desmondtga@arizona.edu>
2024-01-03 18:46:21 -0700

    Refactored to use vite and ts

---
+++ React-App/src/App.tsx
@@ -0,0 +1,7 @@
+import Message from './components/Message';
+
+function App() {
+  return <div><Message></Message></div>
+}
+
+export default App
---
+++ React-App/src/components/Message.tsx
@@ -0,0 +1,5 @@
+function Message() {
+    return <h1>Hello</h1>
+}
+
+export default Message;
