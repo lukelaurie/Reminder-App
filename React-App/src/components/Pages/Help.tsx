2d29bd684400de10e6cfa15471cb7b54ca640817
unknown <lukelaurie13@gmail.com>
2024-06-01 11:42:31 -0700

    finished help page

--- React-App/src/components/Pages/Help.tsx
+++ React-App/src/components/Pages/Help.tsx
@@ -6,6 +6,32 @@ const Help: React.FC = () => {
         <>
             <Header />
             <h1>Help</h1>
+
+            <section>
+                <h2>Appointments Page</h2>
+                <p>
+                    On the appointments page, you can manage your appointments. The functionalities include:
+                </p>
+                <ul>
+                    <li>Clicking a button to fill out a form and add a new appointment.</li>
+                    <li>Clicking a slot on the calendar to automatically add the start time for a new appointment.</li>
+                    <li>Swapping between Month, Week and day view on the calender.</li>
+                    <li>Viewing a list of all your appointments on the calendar.</li>
+                    <li>Clicking on an appointment to see more information about it.</li>
+                    <li>Deleting or updating an appointment if desired.</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>SMS Notifications</h2>
+                <p>
+                    Our appointment manager also includes SMS notifications:
+                </p>
+                <ul>
+                    <li>When an appointment is scheduled, an SMS text message will be sent to the user notifying them of the appointment.</li>
+                    <li>Reminder SMS messages will be sent out 3 days and 1 day before the appointment.</li>
+                </ul>
+            </section>
         </>
     );
 };
--- React-App/src/components/Pages/Home.tsx
+++ React-App/src/components/Pages/Home.tsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
 
 const Home: React.FC = () => {
-    const [dayTime, setDaytime] = useState("Morning");
-    const [userName, setUserName] = useState("name");
-    const [companyName, setCompanyName] = useState("companyName");
+    const [dayTime, setDaytime] = useState("");
+    const [userName, setUserName] = useState("");
+    const [companyName, setCompanyName] = useState("");
     const [appointmentCount, setAppointmentCount] = useState(0);
-    const [nextAppointmentClient, setNextAppointmentClient] = useState(
-        "nextAppointmentClient"
-    );
+    const [nextAppointmentClient, setNextAppointmentClient] = useState("");
     useEffect(() => {
         getUserData();
         getTimeOfDay();
@@ -33,7 +31,6 @@ const Home: React.FC = () => {
     const getTimeOfDay = () => {
         // get the appointments from the start to the end of the day 
         let startDate: Date = new Date();
-        const curHours = startDate.getHours();
         startDate.setHours(0, 0, 0);
         let endDate: Date = new Date();
         endDate.setHours(23, 59, 59, 999);
@@ -41,10 +38,16 @@ const Home: React.FC = () => {
             startDateRange: startDate,
             endDateRange: endDate
         };
+        const curDate: Date = new Date();
+        const curHours: number = startDate.getHours();
         // determine what time of day to show the user
-        // if (curHours < 12) {
-
-        // }
+        if (curHours < 12) {
+            setDaytime("Morning");
+        } else if (curHours < 18) {
+            setDaytime("Afternoon");
+        } else {
+            setDaytime("Evening");
+        }
         fetch("http://127.0.0.1:3000/retrieveAppointments", {
         method: "POST",
         headers: {
@@ -57,8 +60,22 @@ const Home: React.FC = () => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
-            
+            let appointmentTotal: number = 0;
+            let prevSeen: Date | null = null;
+            let clientName: string = "";
+            // find the number of remaining appointments for the day
+            for (let i = 0; i < data.length; i++) {
+                const eventDate = new Date(data[i]["startDate"] * 1000);
+                if (curDate  < eventDate) {
+                    appointmentTotal++;
+                    if (!prevSeen || prevSeen > eventDate) {
+                        prevSeen = eventDate;
+                        clientName = data[i]["clientName"];
+                    }
+                }
+            }
+            setAppointmentCount(appointmentTotal);
+            setNextAppointmentClient(clientName);
         });
     }
     return (
@@ -68,7 +85,7 @@ const Home: React.FC = () => {
             <h1>
                 Good {dayTime}, {userName}!
             </h1>
-            <h2>You have {appointmentCount} appointments today.</h2>
+            <h2>You have {appointmentCount} remaining appointments today.</h2>
             {appointmentCount > 0 && (
                 <h2>Your next appointment is with {nextAppointmentClient}.</h2>
             )}
