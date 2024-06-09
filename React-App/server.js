f0f71ed0abd992375eb222c53e2461998d09debe
Desmondtga <Desmondtga@arizona.edu>
2024-06-09 12:02:38 -0700

    added test route

--- React-App/server.js
+++ React-App/server.js
@@ -4,6 +4,10 @@ const port = 3000
 
 app.use(express.static('dist'));
 
+app.get('/test', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
