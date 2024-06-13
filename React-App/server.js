4b15ae5994b84ab56ef4c934eadd1619b79d65ad
luke laurie <lukelaurie13@gmail.com>
2024-06-13 13:16:19 -0500

    the server

--- React-App/server.js
+++ React-App/server.js
@@ -1,16 +1,18 @@
 import express from 'express';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
-// Serve the React app for all other routes
+// Serve the React app for all routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
